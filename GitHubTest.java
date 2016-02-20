a4d91dc151a714690c99a017c32ddcfd6fe0be7b
JeromeD123 <jerome.daly26@gmail.com>
2016-02-19 22:36:39 -0600

    Update GitHubTest.java

--- GitHubTest.java
+++ GitHubTest.java
@@ -1,5 +1,9 @@
 public class GitHubTest{
   public static void main(Sring[] args){
     System.out.println("This is a test of Github");
+    why();
+  }
+  public static void why(){
+    System.out.println("why the hell didn't it work?")
   }
 }
